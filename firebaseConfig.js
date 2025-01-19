05bbc728816d2ec768193efa610f2c5ec699a690
Vikasini <156224575+VikasiniM@users.noreply.github.com>
2025-01-19 13:45:42 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,30 +1,24 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
-import {getFirestore} from "firebase/firestore"
-import {getDatabase} from "firebase/database"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBkFlbuk1yUK79JjbjuEssiU71HOEuWPWw",
-  authDomain: "testing-fb3fa.firebaseapp.com",
-  projectId: "testing-fb3fa",
-  storageBucket: "testing-fb3fa.appspot.com",
-  messagingSenderId: "1011352025488",
-  appId: "1:1011352025488:web:12f42df290a0553d735b15",
-  measurementId: "G-CCX4HBQ5CJ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const auth = getAuth(app);
-const db =getDatabase(app);
+const db = getDatabase(app);
 const store = getFirestore(app);
 
-
-export {auth,db, store}
+export { auth, db, store };
